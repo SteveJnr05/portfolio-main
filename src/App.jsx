4fc0f8169587fa1dbd66b1ffbd0e1e8c15eb2a3c
SteveJnr05 <swantastephen@gmail.com>
2026-03-06 04:45:46 +0100

    Fix filename capitalization for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/navbar"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Design from "./pages/design"
-import Videography from "./pages/videography"
-import Development from "./pages/development"
+import Design from "./pages/Design"
+import Videography from "./pages/Videography"
+import Development from "./pages/Development"
 import Contact from "./pages/Contact"
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <div className="text-white min-h-screen">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/design" element={<Design />} />
           <Route path="/videography" element={<Videography />} />
